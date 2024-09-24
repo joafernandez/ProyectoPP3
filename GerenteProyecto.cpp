64e44fc77edf0efb38ac777f8decb26fddd187b5
joafernandez <efernandez@fie.undef.edu.ar>
2024-09-23 22:34:52 -0300

    clase gerente-heredera

---
+++ GerenteProyecto.cpp
@@ -0,0 +1,29 @@
+//
+// Created by joana on 23/9/2024.
+//
+
+#include "GerenteProyecto.h"
+#include "Empleado.h"
+#include <iostream>
+
+// implemento constructor
+GerenteProyecto::GerenteProyecto(const string &nombre, const string &puesto, const string &equipo)
+        : Empleado(nombre, puesto), equipoGestionado(equipo) {}
+
+// Getter y Setter
+string GerenteProyecto::getEquipoGestionado() const {
+    return equipoGestionado;
+}
+
+void GerenteProyecto::setEquipoGestionado(const string &equipo) {
+    equipoGestionado = equipo;
+}
+
+// Métodos
+void GerenteProyecto::gestionarProyecto() {
+    cout << "El gerente " << getNombre()<< " esta gestionando el proyecto con su equipo: " << equipoGestionado << endl;
+}
+
+void GerenteProyecto::asignarTareas() {
+    cout << "El gerente " << getNombre ()<< " esta asignando tareas a su equipo." << endl;
+}
---
+++ GerenteProyecto.h
@@ -0,0 +1,29 @@
+//
+// Created by joana on 23/9/2024.
+//
+
+#ifndef GESTORPROYECTOS_GERENTEPROYECTO_H
+#define GESTORPROYECTOS_GERENTEPROYECTO_H
+
+#endif //GESTORPROYECTOS_GERENTEPROYECTO_H
+#include "Empleado.h"
+#include "string"
+using namespace std;
+
+class GerenteProyecto:public Empleado {
+private:
+    string equipoGestionado;
+public:
+   //CONSTR
+    GerenteProyecto(const string &nom, const string &puesto, const string &equipoGest);
+
+    // getters y setters
+
+    string getEquipoGestionado()const;
+    void setEquipoGestionado(const string & equipoGest);
+
+    //metodos:
+    void gestionarProyecto();
+    void asignarTareas();
+};
+
